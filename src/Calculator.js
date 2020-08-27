624eb9501ea0d9d3178974efc35b2f626998e95b
margare-jihua <margaret.jihua@gmail.com>
2020-08-26 21:42:01 -0700

    [+,-,*/] Add function to handle 4 operation

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react'
 
 class Calculator extends Component {
+    
     constructor (props){
         super()
         this.state = {
             num1: '',
             num2: '',
-            sum: 'Addition results go here!'
+            operator: '',
+            result: 'Operation results go here!'
         }
     }
 
@@ -14,34 +16,56 @@ class Calculator extends Component {
         this.setState({ [num]: e.target.value});
     }
 
-    add = () => {
+    setOperator = (e, operator) => {
+        this.setState({ [operator]: e.target.value})
+    }
+
+    calculate = () => {
         let num1 = Number(this.state.num1)
         let num2 = Number(this.state.num2)
-        let sum = num1 + num2
-        this.setState({sum:sum})
+        let operater = this.state.operator
+        if (operater === '+') {
+            let sum = num1 + num2
+            this.setState({result:sum})
+        } else if (operater === '-') {
+            let subtract = num1 - num2
+            this.setState({result:subtract})
+        } else if (operater === '*') {
+            let multiple = num1 * num2
+            this.setState({result:multiple})
+        }else if (operater === '/') {
+            let divide = num1 / num2
+            this.setState({result:divide})
+        }
     }
 
     render() {
         return(
             <div className="container">
-                <h1>Add with React!</h1>
+                <h1>Calculate with React!</h1>
 
-                <div className="add">
+                <div className="calculator">
                     <input type="number"
                         name="num1"
-                        placeholder="Enter your first number"
+                        placeholder="n1"
                         value={this.state.num1}
                         onChange={ (e) => this.setNum(e, 'num1') }
-                    />
-                    <span>+</span>
+                        />
+                    <select onChange={(e) => this.setOperator(e, 'operator')}>
+                        <option value='+'> + </option>
+                        <option value='-'> - </option>
+                        <option value='*'> * </option>
+                        <option value='/'> / </option>
+                    </select>
                     <input type="number"
                         name="num2"
-                        placeholder="Enter your second number"
+                        placeholder="n2"
                         value={this.state.num2}
                         onChange={ (e) => this.setNum(e, 'num2') }
                         />
-                    <button onClick={this.add}>=</button>
-                    <h3>{this.state.sum}</h3>
+                    <button onClick={this.calculate}>=</button>
+
+                    <h2>{this.state.result}</h2>
                 </div>
             </div>
         )
